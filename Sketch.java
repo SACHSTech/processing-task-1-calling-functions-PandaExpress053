a5ed6e4fbf49fb7a4cd2f49297fe80b24a68ad64
PandaExpress053 <ethan.fung25@ycdsbk12.ca>
2024-02-21 14:15:29 -0500

    modified:   Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -8,8 +8,8 @@ public class Sketch extends PApplet {
   /**
    * Called once at the beginning of execution, put your size all in this method
    * Main class to execute sketch
-   *Creates a picture of a house, grass, and a sun
-   * @E.Fung
+   * Creates a picture of a house, grass, and a sun
+   * Author: @E.Fung
    */
   public void settings() {
 	// put your size call here
@@ -30,19 +30,19 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    //Creating green foreground
+    // Creating green foreground
     fill(0, 255, 0);
     rect(0, 300, 400, 400);
 
-    //Creating gray square house base
+    // Creating gray square house base
     fill(150, 150, 150);
     rect(150, 335, 200, -200);
 
-    //Creating white triangle roof
+    // Creating white triangle roof
     fill(255, 255, 255);
     triangle(150, 135, 250, 80, 350, 135);
 
-    //Creating circular yellow sun in top left corner
+    // Creating circular yellow sun in top left corner
     fill(255, 255, 150);
     ellipse(25, 25, 150, 150);
   }
